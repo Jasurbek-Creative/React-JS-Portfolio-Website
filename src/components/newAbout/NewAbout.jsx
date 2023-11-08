97e29e0243aa4761d1e7b970de8273b57fa0b7b3
Jasurbek-Creative <JasurbekJalilov888@gmail.com>
2023-11-08 12:03:25 +0500

    The design parts in the about section have been modified and set to be medium and small responsive

--- src/components/newAbout/NewAbout.jsx
+++ src/components/newAbout/NewAbout.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./newAbout.css";
+import Me from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const NewAbout = () => {
   return (
-    <div>NewAbout</div>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="" />
+          </div>
+        </div>
 
-export default NewAbout
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Project</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+            porro, consectetur sunt nesciunt fugiat sapiente dolor adipisci
+            consequatur pariatur voluptatum enim totam est repellendus. Quae
+            sunt vitae obcaecati. Fugit, sapiente!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NewAbout;
